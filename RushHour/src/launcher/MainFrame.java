3002b56ae773eda7ab6d7a46332c5724626df67b
sgm1 <sgmathai@att.net>
2014-03-06 18:41:54 -0600

    Added a canvas to draw on

--- RushHour/src/launcher/MainFrame.java
+++ RushHour/src/launcher/MainFrame.java
@@ -1,10 +1,29 @@
 package launcher;
 
-public class MainFrame {
+import java.awt.FlowLayout;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class MainFrame extends JFrame implements Runnable{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SwingUtilities.invokeLater(new MainFrame());
+	}
+	
+	public MainFrame(){
+		super("Rush Hour");
+		setLayout(new FlowLayout());
+		MyCanvas temp = new MyCanvas(300, 300);
+		add(temp);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
+	@Override
+	public void run() {
+		pack();
+		setVisible(true);
+		// TODO Auto-generated method stub
+		
+	}
 }
---
+++ RushHour/src/launcher/MyCanvas.java
@@ -0,0 +1,59 @@
+package launcher;
+
+import java.awt.Dimension;
+import java.awt.LayoutManager;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JPanel;
+import javax.swing.event.MouseInputListener;
+
+public class MyCanvas extends JPanel implements MouseInputListener {
+
+	public MyCanvas(int w, int h) {
+		super();
+		this.setPreferredSize(new Dimension(w, h));
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
