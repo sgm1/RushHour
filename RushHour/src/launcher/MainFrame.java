35b47a6987b45a2d4fe0861914a2b1490160ce96
albanes3 <albanes3@uic.edu>
2014-03-11 14:37:20 -0500

    Update MainFrame.java

--- RushHour/src/launcher/MainFrame.java
+++ RushHour/src/launcher/MainFrame.java
@@ -15,10 +15,10 @@ public class MainFrame extends JFrame implements Runnable{
 		super("Rush Hour");
 		setLayout(new FlowLayout());
 		GamePanel temp = new GamePanel(300, 300);// here or run?
-		add(temp);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
-
+		add(temp);				 // we can either make a generic size and resize the tiles, or...
+		setDefaultCloseOperation(EXIT_ON_CLOSE); // ...we can resize the JFrame for each level
+	}						 // Option A is better from a user standpoint but Option B ensures
+							 // that our tiles never get too small
 	@Override
 	public void run() {
 		pack();
