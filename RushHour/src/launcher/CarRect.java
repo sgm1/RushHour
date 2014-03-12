5960097bb0490a9ee55bde95ef8b57077135bd81
albanes3 <albanes3@uic.edu>
2014-03-12 15:11:35 -0500

    Update CarRect.java
    - Added the following fields to the CarRect class:
        size : int
        color : Color
        symbol : char
    
    - CarRect constructor made tile size independent

--- RushHour/src/launcher/CarRect.java
+++ RushHour/src/launcher/CarRect.java
@@ -1,22 +1,31 @@
 package launcher;
 
-import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.util.Random;
+import java.awt.Graphics;
+import java.awt.Color;
 
+@SuppressWarnings("serial")
 public class CarRect extends Rectangle {
-	Color col;
 	
-	public CarRect(int x, int y, int w, int h) {
-		super(x, y, w, h);
-		col = new Color((int)(Math.random() * 256),
-				(int)(Math.random() * 256),
-				(int)(Math.random() * 256));
-		// TODO Auto-generated constructor stub
+	private static int size = 50;
+	private static Random temp = new Random();
+	private Color color;
+	private char symbol;
+	
+	public CarRect(int x, int y, int width, int height, char text) {
+		super(x*size, y*size, width*size, height*size);
+		color = new Color(temp.nextInt(255), temp.nextInt(255), temp.nextInt(255));
+		symbol = text; 
 	}
 	
-	public void draw(Graphics g){
-		g.setColor(col);
-		g.fillRect(this.x, this.y, this.width, this.height);
+	public Color getColor(){
+		return this.color;
 	}
+	
+	public char getSymbol(){
+		return this.symbol;
+	}
+	
+	
 }
