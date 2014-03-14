4b469b3388074ccd8ac5e735e9e7e3e40d19dce5
albanes3 <albanes3@uic.edu>
2014-03-13 19:54:26 -0500

    Update GamePanel.java

--- RushHour/src/launcher/GamePanel.java
+++ RushHour/src/launcher/GamePanel.java
@@ -13,17 +13,20 @@ import javax.swing.event.MouseInputListener;
 import logic.RushHourGame;
 public class GamePanel extends JPanel implements MouseInputListener {
 
-	private int numCars;
+	private int numCars = 4;
 	//private List<CarRect> cars = new ArrayList<CarRect>();
-	private CarRect[] cars;
+	private CarRect[] cars = new CarRect[0];
 	private Point lastPressPoint;
 	private RushHourGame game;//to communicate key events
 								//should communicate on release only
 	private CarRect carPressed;
-
+	private static int tileWidth=10;
+	private static int tileHeight=10;
+	
 	public GamePanel(int w, int h, RushHourGame g) {
 		super();
 		game = g;
+		//CarRect.setTileSize(tileWidth,tileHeight);
 		this.addMouseListener(this);
 		this.addMouseMotionListener(this);
 		this.setPreferredSize(new Dimension(w, h));
@@ -87,7 +90,11 @@ public class GamePanel extends JPanel implements MouseInputListener {
 	@Override
 	public void paint(Graphics g){
 		g.clearRect(0, 0, this.getWidth(), this.getHeight());
-		
+		for(int i=0; i<tileWidth+1;i++)
+			g.drawLine(CarRect.getTileSize()*i, 0, CarRect.getTileSize()*i, CarRect.getTileSize()*tileHeight);
+		for(int i=0; i<tileHeight+1;i++)
+			g.drawLine(0, CarRect.getTileSize()*i, CarRect.getTileSize()*tileWidth,CarRect.getTileSize()*i);
+		System.out.println("" + cars.length);
 		for (CarRect t: cars) {
 			t.draw(g);
 		}
@@ -112,6 +119,14 @@ public class GamePanel extends JPanel implements MouseInputListener {
 
 	public void setCars(ArrayList<CarRect> crs) {
 		cars = (CarRect[]) crs.toArray(new CarRect[0]);
+	}
+	
+	public static int getTileWidth(){
+		return tileWidth;
+	}
+	
+	public static int getTileHeight(){
+		return tileHeight;
 	}
 
 }
