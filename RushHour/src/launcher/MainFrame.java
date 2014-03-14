742c5b2858695b07ee2abe7761f512a03d0831b9
albanes3 <albanes3@uic.edu>
2014-03-13 19:54:07 -0500

    Update MainFrame.java

--- RushHour/src/launcher/MainFrame.java
+++ RushHour/src/launcher/MainFrame.java
@@ -5,31 +5,37 @@ import java.awt.FlowLayout;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
+import launcher.GamePanel;
 import logic.RushHourGame;
 
 public class MainFrame extends JFrame implements Runnable{
+	private static int winWidth = 602;
+	private static int winHeight = 602;
+	
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new MainFrame());
 	}
-	
+
 	public MainFrame(){
 		super("Rush Hour");
 		setLayout(new FlowLayout());
-		RushHourGame daGame = new RushHourGame(300);//constructs square Panel
-		
-		//GamePanel temp = new GamePanel(300, 300);// here or run? 
-							// > Here, but we can make the frame resizeable
-							// we can either make a generic size and resize the tiles, or...
+		RushHourGame daGame = new RushHourGame(602);
 		add(daGame.getPanel());
-		setDefaultCloseOperation(EXIT_ON_CLOSE); // ...we can resize the JFrame for each level
-	}						 // Option A is better from a user standpoint but Option B ensures
-							 // that our tiles never get too small
-							//> I think we can be safe with a 
+		GamePanel temp = new GamePanel(winWidth, winHeight, daGame);			 
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}		
+	
 	@Override
 	public void run() {
 		pack();
 		setVisible(true);
-		// TODO Auto-generated method stub
-		
+	}
+	
+	public static int getWinWidth(){
+		return winWidth;
+	}
+	
+	public static int getWinHeight(){
+		return winHeight;
 	}
 }
