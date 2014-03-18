00be6858dcc50f3a3df629e51d1aa90fc9b59a68
sgm1 <sgmathai@att.net>
2014-03-18 08:15:21 -0500

    Cleaned up a little of teh rushsolver

--- RushHour/src/logic/RushSolver.java
+++ RushHour/src/logic/RushSolver.java
@@ -1,5 +1,7 @@
 package logic;
 
+import java.awt.Point;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 
@@ -8,12 +10,14 @@ public class RushSolver extends Thread{
 	private static HashMap<GridState, GridState> states;//k = curSate, v = Hint
 	private static LinkedList<GridState> toProcess;
 	
-	private static class GridState{//int[][] wrapper
+	private static class GridState {
 		private final int [][] daMap;
+		private HashMap<Point, GridState> neighbors;
 		private int stepsToEnd;
 		
 		public GridState(int [][] gr){
 			daMap = gr.clone();
+			neighbors = new HashMap<Point, GridState>();
 		}
 		
 		@Override
@@ -33,7 +37,7 @@ public class RushSolver extends Thread{
 		}
 		
 		public int[][] getGrid(){
-			return daMap.clone();
+			return daMap.clone();//clone does deep copy (on primitives)
 		}
 	}
 
@@ -63,12 +67,32 @@ public class RushSolver extends Thread{
 	@Override
 	public void run(){
 		while(!toProcess.isEmpty()){
-			enumerate(toProcess.removeLast().getGrid());//clone does deep copy
+			enumerate(toProcess.removeLast().getGrid());
 		}
 	}
 	
+	private boolean canMoveLeft(int[][] gr, int val, int curRow, int curCol, int num){
+		//assumes top left provided
+		//assumes lower "nums" checked
+		if (num < 1)
+			return false;//sanity check
+		if (curCol - num < 0)//too long a stride
+			return false;
+		if (gr[curRow][curCol - num] != 0){//check left
+			return false;
+		}
+		// gr[curRow][curCol - num] must be zero here
+		if (curRow + 1 == gr[0].length || gr[curRow + 1][curCol] != val){
+			return true;
+		}
+		while(gr[curRow + 1][curCol] == val){// check next row
+			return canMoveLeft(gr, val, curRow + 1, curCol, num);
+		}
+		return false;
+	}
+	
 	/**
-	 * Provided the top right sector of the value 'val',
+	 * Provided the top lect sector of the value 'val',
 	 * decides if piece can move 'num' spaces 
 	 * (Should support any size rectangles)
 	 * 
@@ -80,20 +104,27 @@ public class RushSolver extends Thread{
 	 * @return
 	 */
 	private boolean canMoveRight(int[][] gr, int val, int curRow, int curCol, int num){
-		if (curRow + 1 == gr.length)//already at edge
-			return false;
-		if (gr[curRow + 1][curCol] == val)// to far left in block
-			return canMoveRight(gr, val, curRow + 1, curCol, num);
-		//base case
-		if (gr[curRow + 1][curCol] == 0 &&
-				(curCol + 1 == gr[0].length //at bot edge
-				|| gr[curRow][curCol + 1] != val)){// at bot right
-			return gr[curRow + num][curCol] == 0;//return can move
+		//assumes top left provided
+		//assumes lower "nums" checked
+		if (num < 1)
+			return false;//sanity check
+		while (curCol + 1 < gr[0].length &&
+				gr[curRow][curCol + 1] == val){ // too far left in block
+			++curCol;// traverse right
+			//TODO re-check
 		}
-		if (gr[curRow + 1][curCol] == 0 &&// at the right side
-				gr[curRow + num][curCol] == 0//can move that 'num' spaces
-				&& ( gr[curRow][curCol + 1] == val)){//there is another row
-			return canMoveRight(gr, val, curRow, curCol + 1, num);//curRow free, check next level
+		if (curCol + num >= gr[0].length)//too long a stride
+			return false;
+		//base case
+		if (gr[curRow][curCol + num] == 0 && // free to move
+				(curRow + 1 == gr[0].length //at bot edge
+				|| gr[curRow + 1][curCol] != val)){// at bot right
+			return true;//return can move
+		}//TODO Make these V and ^ loop? 
+		if (gr[curRow][curCol + num] == 0 &&// free to move
+				(gr[curRow + 1][curCol] == val)){//there is another row
+			return canMoveRight(gr, val, curRow + 1, curCol, num);//curRow free
+																//check next level
 		}
 		return false;//else can't move, base case 2
 	}
@@ -108,6 +139,23 @@ public class RushSolver extends Thread{
 	private void tryMoveRight(int[][] gr, int val, int x, int y, int dir){
 		if (dir == 1)
 			return;
+		int numSpaces = 1;
+		int height = gr.length, width = gr[0].length;
+		int [][] temp;
+		while(canMoveRight(gr, val, x, y, numSpaces)){
+			temp = gr.clone();
+			for (int j = 0; j < height; ++j){
+				for (int k = width - 1; k >= 0; --k){//right to left to prevent overwrite
+					if (gr[j][k] == val){//"map" original into temp transformed
+						temp[j][k] = 0;
+						temp[j][k + numSpaces] = gr[j][k];
+					}
+				}
+			}
+			//add new grid
+			
+			++numSpaces;
+		}
 		// parse right
 		// move down to check width
 	}
@@ -129,7 +177,7 @@ public class RushSolver extends Thread{
 		boolean [] indexIsDone = new boolean[dirs.length];//initializes to false
 		int width = gr[0].length, height = gr.length;
 		int val;
-		for (int j = 0; j < height; ++j){//fix the 2 for loops positions
+		for (int j = 0; j < height; ++j){
 			for (int k = 0; k < width; ++k){
 				val = gr[j][k];
 				if (val > 0 && !indexIsDone[val]){
