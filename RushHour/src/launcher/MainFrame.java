88b0b38a479df20eb11ade352828ee3ff6725468
sgm1 <sgmathai@att.net>
2014-03-24 12:18:46 -0500

    tookout null charater

--- RushHour/src/launcher/MainFrame.java
+++ RushHour/src/launcher/MainFrame.java
@@ -36,7 +36,7 @@ public class MainFrame extends JFrame implements Runnable{
 	private static boolean closeGame = false;
 	private static int carsMade;
 	private static RushHourGame daGame;
-	private static final char[] extraChars = {'\0', '\1', '\2', '\3', '\4', '\5', '\6', '\7', '\10', '\11', '\12', '\13', '\14', '\15'
+	private static final char[] extraChars = {'\1', '\2', '\3', '\4', '\5', '\6', '\7', '\10', '\11', '\12', '\13', '\14', '\15'
 		, '\16', '\17', '\20', '\21', '\22', '\23', '\24', '\25', '\26', '\27', '\30', '\31', '\32', '\33'
 		, '\34', '\35', '\36', '\37', '\40', '\41', '\42', '\43', '\44', '\45', '\46', '\47', '\50', '\51'
 		, '\52', '\53', '\54', '\55', '\56', '\57', '\60', '\61', '\62', '\63', '\64', '\65', '\66', '\67'
